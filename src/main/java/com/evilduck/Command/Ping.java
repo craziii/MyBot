f48e07208c3bc89ed807a138f85160a8ce94fc3e
evilduck95 <keiranbrown95@gmail.com>
2019-01-22 22:21:17 +0000

    Refactored color code

--- src/main/java/com/evilduck/Command/Ping.java
+++ src/main/java/com/evilduck/Command/Ping.java
@@ -9,16 +9,11 @@ import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
-import java.awt.*;
+import java.awt.Color;
 
 import static java.lang.String.valueOf;
 
 @Component
-public class Ping implements GenericCommand {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Ping.class);
-
-    @Component
 public class Ping implements GenericCommand {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Ping.class);
@@ -30,48 +25,35 @@ public class Ping implements GenericCommand {
         final TextChannel originTextChannel = message.getPayload().getTextChannel();
         final long pingValue = message.getPayload().getJDA().getPing();
 
-        String pingText = "";
-        byte pingColorR = 0;
-        byte pingColorG = 0;
-        byte pingColorB = 0;
-        String pingIcon = "";
+        final String pingText;
+        final Color pingColor;
+        final String pingIcon;
 
-        if(pingValue < 100){
+        if (pingValue < 100) {
             pingText = "This is definitely a good thing";
-            pingColorR = 34;
-            pingColorG = 139;
-            pingColorB = 34;
+            pingColor = new Color(34, 139, 34);
             pingIcon = "https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg";
-        }
-        else if(pingValue < 250){
+        } else if (pingValue < 250) {
             pingText = "This is possibly a small hiccup, but if it persists I'm getting my umbrella";
-            pingColorR = 255;
-            pingColorG = 252;
-            pingColorB = 127;
+            pingColor = new Color(255, 252, 127);
             pingIcon = "https://i.imgur.com/OsfBMXz.png";
-        }
-        else if(pingValue < 500){
+        } else if (pingValue < 500) {
             pingText = "This is possibly a small hiccup, but if it persists its getting to be a pretty bad thing";
-            pingColorR = 255;
-            pingColorG = 126;
-            pingColorB = 71;
+            pingColor = new Color(255, 126, 71);
             pingIcon = "https://i.imgur.com/22oFoi2.png";
-        }
-        else{
+        } else {
             pingText = "This is a bad sign, I gotta be honest";
-            pingColorR = 255;
-            pingColorG = 0;
-            pingColorB = 0;
+            pingColor = new Color(255, 0, 0);
             pingIcon = "https://discordapp.com/assets/15ccaf984f2fafcf3ed5d896763ed510.svg";
         }
 
-            originTextChannel.sendMessage(new EmbedBuilder()
-                    .setAuthor("Ping", pingIcon)
-                    .setColor(pingColorR, pingColorG, pingColorB)
-                    .addField("Value (ms)", valueOf(pingValue), true)
-                    .addField("", pingText, false)
-                    .build())
-                    .queue();
+        originTextChannel.sendMessage(new EmbedBuilder()
+                .setAuthor("Ping", pingIcon)
+                .setColor(pingColor)
+                .addField("Value (ms)", valueOf(pingValue), true)
+                .addField("", pingText, false)
+                .build())
+                .queue();
     }
 
     @Override

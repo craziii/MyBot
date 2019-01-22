b8cf0829468518fb9ba2d53b0708e30965a1f759
evilduck95 <keiranbrown95@gmail.com>
2019-01-22 21:51:35 +0000

    Refactored PR, now up to date with JDA lib

--- src/main/java/com/evilduck/Command/Ping.java
+++ src/main/java/com/evilduck/Command/Ping.java
@@ -9,6 +9,10 @@ import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+
+import static java.lang.String.valueOf;
+
 @Component
 public class Ping implements GenericCommand {
 
@@ -17,52 +21,52 @@ public class Ping implements GenericCommand {
     @Override
     @ServiceActivator(inputChannel = "pingChannel")
     public void execute(final Message<net.dv8tion.jda.core.entities.Message> message) {
-        LOGGER.info("Recieved: {}", message.getPayload().getContentRaw());
+        LOGGER.info("Received: {}", message.getPayload().getContentRaw());
         final TextChannel originTextChannel = message.getPayload().getTextChannel();
         final long pingValue = message.getPayload().getJDA().getPing();
 
-        if(pingValue < 100){
+        if (pingValue < 100) {
             originTextChannel.sendMessage(new EmbedBuilder()
-                .withAuthorName("Ping")
-                .setColor(0,255,0)
-                .withAuthorIcon("https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
-                .appendField("Value (ms)", String.valueOf(pingValue), true)
-                .appendField("", "This is definitely a good thing", false)
-                .build())
-                .queue();
+                    .setAuthor("Ping", "https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
+                    .setColor(new Color(0, 255, 0))
+                    .addField("Value (ms)", valueOf(pingValue), true)
+                    .addField("", "This is definitely a good thing", false)
+                    .build())
+                    .queue();
+        } else if (pingValue > 100 && pingValue <= 250) {
+            originTextChannel.sendMessage(new EmbedBuilder()
+                    .setAuthor("Ping", "https://i.imgur.com/OsfBMXz.png")
+                    .setColor(new Color(255, 255, 0))
+                    .addField("Value (ms)", valueOf(pingValue), true)
+                    .addField("", "This is possibly a small hiccup, but if it persists I'm getting my umbrella", false)
+                    .build())
+                    .queue();
+        } else if (pingValue > 250 && pingValue <= 500) {
+            originTextChannel.sendMessage(new EmbedBuilder()
+                    .setAuthor("Ping", "https://i.imgur.com/22oFoi2.png")
+                    .setColor(new Color(255, 165, 0))
+                    .addField("Value (ms)", valueOf(pingValue), true)
+                    .addField("", "This is possibly a small hiccup, but if it persists its getting to be a pretty bad thing", false)
+                    .build())
+                    .queue();
+        } else {
+            originTextChannel.sendMessage(new EmbedBuilder()
+                    .setAuthor("Ping", "https://discordapp.com/assets/15ccaf984f2fafcf3ed5d896763ed510.svg")
+                    .setColor(new Color(255, 0, 0))
+                    .addField("Value (ms)", valueOf(pingValue), true)
+                    .addField("", "This is a bad sign, I gotta be honest", false)
+                    .build())
+                    .queue();
         }
+    }
 
-        if(pingValue > 100 && pingValue <= 250){
-            originTextChannel.sendMessage(new EmbedBuilder()
-                .withAuthorName("Ping")
-                .setColor(255,255,0)
-                .withAuthorIcon("https://i.imgur.com/OsfBMXz.png")
-                .appendField("Value (ms)", String.valueOf(pingValue), true)
-                .appendField("", "This is possibly a small hiccup, but if it persists I'm getting my umbrella", false)
-                .build())
-                .queue();
-        }
+    @Override
+    public void onSuccess() {
 
-        if(pingValue > 250 && pingValue <= 500){
-            originTextChannel.sendMessage(new EmbedBuilder()
-                .withAuthorName("Ping")
-                .setColor(255,165,0)
-                .withAuthorIcon("https://i.imgur.com/22oFoi2.png")
-                .appendField("Value (ms)", String.valueOf(pingValue), true)
-                .appendField("", "This is possibly a small hiccup, but if it persists its getting to be a pretty bad thing", false)
-                .build())
-                .queue();
-        }
+    }
 
-        if(pingValue > 500){
-            originTextChannel.sendMessage(new EmbedBuilder()
-                .withAuthorName("Ping")
-                .setColor(255,0,0)
-                .withAuthorIcon("https://discordapp.com/assets/15ccaf984f2fafcf3ed5d896763ed510.svg")
-                .appendField("Value (ms)", String.valueOf(pingValue), true)
-                .appendField("", "This is a bad sign, I gotta be honest", false)
-                .build())
-                .queue();
-        }
+    @Override
+    public void onFail() {
+
     }
 }
---
+++ src/test/java/com/evilduck/Command/KickTest.java
@@ -0,0 +1,30 @@
+package com.evilduck.Command;
+
+import net.dv8tion.jda.core.entities.Message;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KickTest {
+
+    @Mock private Message testMessage;
+
+    @InjectMocks private Kick kick;
+
+    @Test
+    void execute() {
+
+
+
+    }
+
+    @Test
+    void onSuccess() {
+    }
+
+    @Test
+    void onFail() {
+    }
+}

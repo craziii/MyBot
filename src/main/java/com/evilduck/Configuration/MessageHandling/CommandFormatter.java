5d6787eed88097d5b613026067aeb376db428a34
Keiran <keiranbrown95@gmail.com>
2019-04-15 21:11:39 +0100

    Changed CommandFormatter, it can now replace self mention at beginning of message with prefix.

--- src/main/java/com/evilduck/Configuration/MessageHandling/CommandFormatter.java
+++ src/main/java/com/evilduck/Configuration/MessageHandling/CommandFormatter.java
@@ -1,24 +1,85 @@
 package com.evilduck.Configuration.MessageHandling;
 
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.SelfUser;
+import net.dv8tion.jda.core.entities.impl.ReceivedMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+import static java.lang.Long.parseLong;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.springframework.messaging.support.MessageBuilder.withPayload;
 
 @Component
 public class CommandFormatter {
 
+    private final JDA jda;
+
+    @Autowired
+    public CommandFormatter(final JDA jda) {
+        this.jda = jda;
+    }
 
     @Transformer
     public Message<net.dv8tion.jda.core.entities.Message> transform(final Message<net.dv8tion.jda.core.entities.Message> message) {
-        return withPayload(message.getPayload())
-                .setHeader("args", getCommandArgs(message.getPayload().getContentRaw()))
+
+        final net.dv8tion.jda.core.entities.Message payload = message.getPayload();
+        final String selfMention = jda.getSelfUser().getAsMention();
+        final String newContent = payload.getContentRaw().replaceFirst(selfMention + " ?", "!");
+        final MutableMessage newMessage = new MutableMessage(payload, newContent);
+
+        return withPayload((net.dv8tion.jda.core.entities.Message) newMessage)
+                .setHeader("args", getCommandArgs(payload.getContentRaw()))
                 .build();
     }
 
     private static int getCommandArgs(final String commandString) {
         return commandString.replace("!", "").split(" ").length;
+    }
+
+    private boolean isSelfMentioned(final net.dv8tion.jda.core.entities.Message payload) {
+        final SelfUser selfUser = jda.getSelfUser();
+        final List<Member> mentionedMembers = payload.getMentionedMembers();
+
+        return mentionedMembers.size() == 1 &&
+                mentionedMembers.stream()
+                        .findFirst()
+                        .get()
+                        .getUser()
+                        .getDiscriminator().equals(selfUser.getDiscriminator());
+    }
+
+    private class MutableMessage extends ReceivedMessage {
+
+        public ReceivedMessage getAsReceivedMessage(final net.dv8tion.jda.core.entities.Message message,
+                                                    final String newContent) {
+            return new MutableMessage(message, newContent);
+        }
+
+        private MutableMessage(final net.dv8tion.jda.core.entities.Message message,
+                               final String newContent) {
+            super(parseLong(message.getId()),
+                    message.getChannel(),
+                    message.getType(),
+                    message.isWebhookMessage(),
+                    message.mentionsEveryone(),
+                    message.isTTS(),
+                    message.isPinned(),
+                    isBlank(newContent) ? message.getContentRaw() : newContent,
+                    message.getNonce(),
+                    message.getAuthor(),
+                    message.getEditedTime(),
+                    message.getReactions(),
+                    message.getAttachments(),
+                    message.getEmbeds());
+        }
+
+
     }
 
 }

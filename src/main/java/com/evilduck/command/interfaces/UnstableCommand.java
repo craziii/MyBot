0c9ff56b70eaa8444ce8817b58b1f5ec7474e9a2
Keiran <keiranbrown95@gmail.com>
2019-08-18 01:21:09 +0100

    Added defaults to Unstable Command to cleanup code

--- src/main/java/com/evilduck/command/interfaces/UnstableCommand.java
+++ src/main/java/com/evilduck/command/interfaces/UnstableCommand.java
@@ -2,11 +2,19 @@ package com.evilduck.command.interfaces;
 
 
 import net.dv8tion.jda.core.entities.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface UnstableCommand extends GenericCommand {
 
-    void onSuccess(Message message);
+    Logger LOGGER = LoggerFactory.getLogger(UnstableCommand.class);
 
-    void onFail(Throwable throwable);
+    default void onSuccess(final Message message) {
+        LOGGER.info("Command was successful from mesage: {}", message.getContentRaw());
+    }
+
+    default void onFail(final Throwable throwable) {
+        LOGGER.warn("Command was unsuccessful! The following error occured: {}", throwable.getMessage());
+    }
 
 }
